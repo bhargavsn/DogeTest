6493a43836e73fb098811038e83a12370db304d5
bhargavsn <bhargavsn@gmail.com>
2021-11-02 02:19:42 +0800

    added parsing data capability

--- ReadCrypPanic.py
+++ ReadCrypPanic.py
@@ -37,13 +37,74 @@ class CryptoNews:
 
 def get_crypto_data():
     url = "https://cryptopanic.com/api/posts/?auth_token"
-    auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # Replace with your correct token
+    auth_token = "xxxxxx"  # Replace with your correct token
     crypto_obj = CryptoNews(url, auth_token)
     data = crypto_obj.load_data()
     return data
 
 
+def analyse_data(data, currency):
+    k = {'Currency': currency, 'Score news': 0}
+    j = 0
+    while j < len(data['results']):
+        # print("Scanning the news...")
+        # print(data['results'][j])
+        try:
+            for x in data['results'][j]['currencies']:
+                # print(x['code'])
+                if k['Currency'] == x['code']:
+                    important_votes = float(data['results'][j]['votes']['important'])
+                    if important_votes == 0:
+                        important_votes = 1
+
+                    negative_votes = float(data['results'][j]['votes']['negative'])
+                    if negative_votes == 0:
+                        negative_votes = 1
+
+                    positive_votes = float(data['results'][j]['votes']['positive'])
+                    if positive_votes == 0:
+                        positive_votes = 1
+
+                    liked_votes = float(data['results'][j]['votes']['liked'])
+                    if liked_votes == 0:
+                        liked_votes = 1
+
+                    disliked_votes = float(data['results'][j]['votes']['disliked'])
+                    if disliked_votes == 0:
+                        disliked_votes = 1
+
+                    toxic_votes = float(data['results'][j]['votes']['toxic'])
+                    if toxic_votes == 0:
+                        toxic_votes = 1
+
+                    # print("Found news on " + k['Currency'] + " calculating news score...")
+                    k[
+                        'Score news'] += important_votes / negative_votes * positive_votes * liked_votes / disliked_votes / toxic_votes
+
+        except Exception as e:
+            print("")
+        j += 1
+    print(k)
+
+
+def print_ccy_codes(data):
+    ccy_list = []
+    j = 0
+    while j < len(data['results']):
+        try:
+            for x in data['results'][j]['currencies']:
+                if x['code'] not in ccy_list:
+                    ccy_list.append(x['code'])
+
+        except Exception as e:
+            print("")
+        j += 1
+    print(ccy_list)
+
+
 # Program Entry point
 if __name__ == '__main__':
     crypto_data = get_crypto_data()
-    print(crypto_data)
+    print_ccy_codes(crypto_data)
+    analyse_data(crypto_data, "DOGE")
+    analyse_data(crypto_data, "BTC")
