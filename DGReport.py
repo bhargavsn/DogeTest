cc093bcc858b589d4a73acaa4c7364626596271c
bhargavsn <bhargavsn@gmail.com>
2021-11-10 13:05:04 +0800

    Boilerplate version

---
+++ DGReport.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Bhargav
+# ---------------------------------------------------------------------------
+""" Util Functions to send quick reports and Emails"""
+
+
+def send_text_email(from_addr: str, to_addr: str, msg: str):
+    """ To do: implementation"""
+
+
+def send_tabulated_email(from_addr: str, to_addr: str, msg):
+    """ To do: implementation"""
